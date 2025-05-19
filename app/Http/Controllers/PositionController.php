93e18022386beabf87df8452a09a31671d2f226b
alexkaasik <aleks.kaasik@outlook.com>
2025-05-19 18:47:33 +0300

    Adding a PositionContoller and adding a simple function to send 200 code, that positions roles.

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PositionController extends Controller
+{
+    public function GetPositions()
+    {
+        $positions = [
+            ['id' => 1, 'name' => 'Lawyer'],
+            ['id' => 2, 'name' => 'Content manager'],
+            ['id' => 3, 'name' => 'Security'],
+            ['id' => 4, 'name' => 'Designer'],
+        ];
+        
+        return response()->json(['success'=> true, 'positions' => $positions], 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PositionController;
 
+Route::get('/positions', [PositionController::class, 'GetPositions']);
