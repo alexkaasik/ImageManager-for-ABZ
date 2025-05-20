eb68e64bff70104a461e208800c91397e015f750
alexkaasik <aleks.kaasik@outlook.com>
2025-05-20 16:16:52 +0300

    Adding Seeder functions for generations for test users

---
+++ database/seeders/TestUserGeneration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+class TestUserGeneration extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 45) as $i) {
+            User::create([
+                'FullName' => $faker->name,
+                'E-Mail' => $faker->unique()->safeEmail,
+                'Phone' => $faker->unique()->phoneNumber,
+                'PositionId' => rand(1, 4),
+            ]);
+        }
+    }
+}
